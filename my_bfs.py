8dd1adf9489c786729f12e2a821568ccb49400e4
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-04-06 20:54:48 +0200

    bfs done

--- my_bfs.py
+++ my_bfs.py
@@ -9,7 +9,6 @@ def bfs(start_board, directions):
     rows = column = len(start_board)
     # history tracks the trace of the algorithm
     history = []
-
     if mf.is_goal(start_board, rows):
         return True, start_board
     queue = deque()
@@ -18,27 +17,29 @@ def bfs(start_board, directions):
     queue.append(start_board)
     # adding first element to the visited elements
     visited_elements.add(str(start_board))
-
     while len(queue) != 0:
         current_element = queue.popleft()
-
+        # adding next element to the history
+        history.append(current_element)
+        # generating neighbours
         neighbors_current_element = mf.generate_neighbours(current_element, rows, column, directions)
-
         for neighbor in neighbors_current_element:
-
             if str(neighbor) not in visited_elements:
                 if mf.is_goal(neighbor, rows):
+                    # adding last element to the history
+                    history.append(neighbor)
                     return True, history
+                # adding next element to the queue
                 queue.append(neighbor)
+                # adding next element to the visited elements
                 visited_elements.add(str(neighbor))
-
     return False
 
 
 # test
-board, rows, cols = mf.import_board("./plansze/4x4G7/4x4_02_00001.txt")
-result, history = bfs(board, "UDLR")
-print("history:")
-for el in history:
-    mf.print_board(board)
-    print()
+# board, rows, cols = mf.import_board("./plansze/4x4G7/4x4_04_00001.txt")
+# result, history = bfs(board, "DLUR")
+# print("history:")
+# for el in history:
+#     mf.print_board(el)
+#     print()
