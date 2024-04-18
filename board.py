1e902185b31b425a62b7fa1e5bd14c95012553b2
arkadiuso000 <arkadiusz.mlyn@icloud.com>
2024-04-19 00:50:51 +0200

    class board added

---
+++ board.py
@@ -0,0 +1,21 @@
+class Board:
+
+    def __init__(self, plain_board, rows, columns, history, how_created):
+        self.board = plain_board
+        self.rows = rows
+        self.columns = columns
+        self.history = history
+        if how_created is not None:
+            if history == "":
+                self.history += "{}".format(how_created)
+            else:
+                self.history += "-{}".format(how_created)
+
+    def __hash__(self):
+        # converts variable board to tuple to allow adding to the set
+        return hash(tuple(self.board))
+
+    def __eq__(self, other):
+        if isinstance(other, Board):
+            return self.board == other.board
+        return False
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+from board import Board
+
+b1 = Board([1,2,3],1,3,"","prawo")
+print(b1.history)
+
+b2 = Board([1,2,3],b1.rows,b1.columns,b1.history,"gora")
+b3 = Board([1,2,3],b1.rows,b1.columns,b1.history,"dol")
+print(b2.history)
+
+a = set()
+a.add(b1)
+a.add(b2)
+a.add(b3)
+print(a)
